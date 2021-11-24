7b97ef132d1478d1fc0d9ad923e5eb532620cece
Mrinal Kukreti <mrinalkukreti@gmail.com>
2021-11-24 19:11:08 +0530

    trying to debug

--- src/index.ts
+++ src/index.ts
@@ -73,100 +73,6 @@ const symbolsByPk = Object.assign(
   ...Object.entries(nativeMarketsV3).map(([a, b]) => ({ [b]: a }))
 )
 
-async function collectEventQueue(m: MarketConfig, r: RedisConfig) {
-  try {
-    const store = await createRedisStore(r, m.marketName)
-    const marketAddress = new PublicKey(m.marketPk)
-    const programKey = new PublicKey(m.programId)
-    const connection = new Connection(m.clusterUrl)
-    const market = await Market.load(
-      connection,
-      marketAddress,
-      undefined,
-      programKey
-    )
-
-    async function fetchTrades(
-      lastSeqNum?: number
-    ): Promise<[Trade[], number]> {
-      const now = Date.now()
-      const accountInfo = await connection.getAccountInfo(
-        market['_decoded'].eventQueue
-      )
-      if (accountInfo === null) {
-        throw new Error(
-          `Event queue account for market ${m.marketName} not found`
-        )
-      }
-      const { header, events } = decodeRecentEvents(
-        accountInfo.data,
-        lastSeqNum
-      )
-      const takerFills = events.filter(
-        (e) => e.eventFlags.fill && !e.eventFlags.maker
-      )
-      const trades = takerFills
-        .map((e) => market.parseFillEvent(e))
-        .map((e) => {
-          return {
-            price: e.price,
-            side: e.side === 'buy' ? TradeSide.Buy : TradeSide.Sell,
-            size: e.size,
-            ts: now,
-          }
-        })
-      /*
-    if (trades.length > 0)
-      console.log({e: events.map(e => e.eventFlags), takerFills, trades})
-    */
-      return [trades, header.seqNum]
-    }
-
-    async function storeTrades(ts: Trade[]) {
-      if (ts.length > 0) {
-        console.log(m.marketName, ts.length)
-        for (let i = 0; i < ts.length; i += 1) {
-          await store.storeTrade(ts[i])
-        }
-      }
-    }
-
-    while (true) {
-      try {
-        const lastSeqNum = await store.loadNumber('LASTSEQ')
-        const [trades, currentSeqNum] = await fetchTrades(lastSeqNum)
-        storeTrades(trades)
-        store.storeNumber('LASTSEQ', currentSeqNum)
-      } catch (e) {
-        notify(`collectEventQueue ${m.marketName} ${e.toString()}`)
-      }
-      await sleep({ Seconds: fetchInterval })
-    }
-  } catch (e) {
-    notify(`collectEventQueue ${m.marketName} ${e.toString()}`)
-  }
-}
-
-function collectMarketData(programId: string, markets: Record<string, string>) {
-  if (process.env.ROLE === 'web') {
-    console.warn('ROLE=web detected. Not collecting market data.')
-    return
-  }
-
-  Object.entries(markets).forEach((e) => {
-    const [marketName, marketPk] = e
-    const marketConfig = {
-      clusterUrl,
-      programId,
-      marketName,
-      marketPk,
-    } as MarketConfig
-    collectEventQueue(marketConfig, { host, port, password, db: 0 })
-  })
-}
-
-collectMarketData(programIdV3, nativeMarketsV3)
-
 const groupConfig = Config.ids().getGroup('mainnet', 'mainnet.1') as GroupConfig
 
 async function collectPerpEventQueue(r: RedisConfig, m: PerpMarketConfig) {
@@ -291,7 +197,7 @@ Object.keys(priceScales).forEach((marketName) => {
 
   const store = new RedisStore(conn, marketName)
   marketStores[marketName] = store
-  console.log("object keys was called" + marketStores[marketName]);
+  // console.log("object keys was called" + JSON.stringify(marketStores[marketName]));
 
   // preload heavy markets
   if (['SOL/USDC', 'SOL-PERP', 'BTC-PERP'].includes(marketName)) {
@@ -381,7 +287,7 @@ app.get('/tv/history', async (req, res) => {
     from = Math.floor(from / resolution) * resolution
     to = Math.ceil(to / resolution) * resolution
 
-    console.log("candle Load error Log" + marketStores[marketName]);
+    // console.log("candle Load error Log" + marketStores[marketName]);
 
     // ensure the candle is at least one period in length
     if (from == to) {
@@ -401,57 +307,13 @@ app.get('/tv/history', async (req, res) => {
     res.send(response)
     return
   } catch (e) {
-    console.log(e)
+    // console.log(e)
     notify(`tv/history ${marketName} ${e.toString()}`)
     const error = { s: 'error candle load error' }
     res.status(500).send(error)
   }
 })
 
-app.get('/trades/address/:marketPk', async (req, res) => {
-  // parse
-  const marketPk = req.params.marketPk as string
-  const marketName =
-    symbolsByPk[marketPk] ||
-    groupConfig.perpMarkets.find((m) => m.publicKey.toBase58() === marketPk)
-      ?.name
-
-  // validate
-  const validPk = marketName != undefined
-  if (!validPk) {
-    const error = { s: 'error', validPk }
-    res.status(404).send(error)
-    return
-  }
-
-  // respond
-  try {
-    const store = marketStores[marketName] as RedisStore
-    const trades = await store.loadRecentTrades()
-    const response = {
-      success: true,
-      data: trades.map((t) => {
-        return {
-          market: marketName,
-          marketAddress: marketPk,
-          price: t.price,
-          size: t.size,
-          side: t.side == TradeSide.Buy ? 'buy' : 'sell',
-          time: t.ts,
-          orderId: '',
-          feeCost: 0,
-        }
-      }),
-    }
-    res.set('Cache-control', 'public, max-age=5')
-    res.send(response)
-    return
-  } catch (e) {
-    notify(`trades ${marketName} ${e.toString()}`)
-    const error = { s: 'error, last section error', e}
-    res.status(500).send(error)
-  }
-})
 
 const httpPort = parseInt(process.env.PORT || '5000')
 app.listen(httpPort)
