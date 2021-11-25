1b62b690244cad661366f6acf77d669adca9d243
Mrinal Kukreti <mrinalkukreti@gmail.com>
2021-11-25 12:38:03 +0530

    added all markets

--- src/index.ts
+++ src/index.ts
@@ -346,7 +346,19 @@ Object.keys(priceScales).forEach((marketName) => {
 })
 
 const app = express()
-app.use(cors())
+
+var whitelist = ['https://nodebunch.finance/']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions))
 
 app.get('/tv/config', async (req, res) => {
   const response = {
